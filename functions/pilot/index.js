353b9423562663b6154087ff3ecdb369bdd31271
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-11-20 17:04:07 -0300

    Adjust .gitignore

--- functions/pilot/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
-    res.status(200).send('Esta es la');
-};
-//# sourceMappingURL=index.js.map
